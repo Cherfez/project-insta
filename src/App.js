a11d825ca6eb208f0a7fccb16cdf712900c9df33
Cherfez <chervasev.leeuwen@gmail.com>
2020-02-25 09:13:59 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 
+//make components for everything (different files to seperate)
+//add components in APP function
 export default function App() {
   return <div className="container"></div>;
 }
